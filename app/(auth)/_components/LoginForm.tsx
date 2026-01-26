5233a453216ac8e3704d52aa1a6d2688cdb24187
Jahongir Hayitov <Jahongirhacking@gmail.com>
2026-01-26 13:53:55 +0500

    feat: add one id button to auth

--- app/(auth)/_components/LoginForm.tsx
+++ app/(auth)/_components/LoginForm.tsx
@@ -4,56 +4,77 @@ import { signInAction } from "@/app/(auth)/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import paths from "@/lib/paths";
+import { Loader, LogIn } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
+import { FormEvent, useTransition } from "react";
 import { toast } from "sonner";
 
 enum FormItems {
-  Login = "login",
-  Password = "password",
+    Login = "login",
+    Password = "password",
 }
 
 export default function LoginForm() {
-  const router = useRouter();
+    const router = useRouter();
+    const [pending, startTransition] = useTransition();
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    try {
-      const result = await signInAction(formData);
-      if (result?.success) {
-        toast.success("Muvaffaqiyatli kirildi!");
-        router.replace(paths.private.dashboard);
-      } else if (result?.error) {
-        toast.error(String(result?.error));
-      }
-    } catch (err) {
-      console.error(err);
-      toast.error((err as { message: string })?.message);
-    }
-  };
+        const formData = new FormData(e.currentTarget);
+        startTransition(async () => {
+            try {
+                const result = await signInAction(formData);
+                toast.dismiss();
+                if (result?.success) {
+                    toast.success("Muvaffaqiyatli kirildi!");
+                    router.replace(paths.private.dashboard); // non-urgent
+                } else if (result?.error) {
+                    toast.error(String(result?.error));
+                }
+            } catch (err) {
+                console.error(err);
+                toast.error((err as { message: string })?.message);
+            }
+        });
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-4">
-          <label htmlFor={FormItems.Login}>
-            Login
-            <Input id={FormItems.Login} name={FormItems.Login} />
-          </label>
-          <label htmlFor={FormItems.Password}>
-            Parol
-            <Input
-              id={FormItems.Password}
-              name={FormItems.Password}
-              type="password"
-              required
-            />
-          </label>
-        </div>
-        <Button type="submit">Kirish</Button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-4">
+                    <label htmlFor={FormItems.Login}>
+                        Login
+                        <Input id={FormItems.Login} name={FormItems.Login} />
+                    </label>
+                    <label htmlFor={FormItems.Password}>
+                        Parol
+                        <Input
+                            id={FormItems.Password}
+                            name={FormItems.Password}
+                            type="password"
+                            required
+                        />
+                    </label>
+                </div>
+
+                <div className="bg-[var(--background)] flex flex-wrap justify-center gap-5 p-4 rounded-[8px]">
+                    <Image src={'/images/text.png'} width={90} height={30} alt="recaptcha" />
+                    <Input placeholder="Tekshiruv kodini kiriting" className="flex-1 max-w-[250px]" />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <Button type="submit" disabled={pending}>
+                        {pending ? <Loader className="animate-spin" /> : <LogIn />} Kirish
+                    </Button>
+                    <a href="#">
+                        <Button className="w-full">
+                            <Image src={'/icons/one-id.svg'} width={32} height={21} alt="One ID" /> OneID orqali kirish
+                        </Button>
+                    </a>
+                </div>
+            </div>
+        </form>
+    );
 }
--- app/cabinet/_components/header/LogoutButton.tsx
+++ app/cabinet/_components/header/LogoutButton.tsx
@@ -7,18 +7,19 @@ import { LogoutIcon } from "@/public/icons";
 import { useTransition } from "react";
 
 export function LogoutButton() {
-  const [pending, startTransition] = useTransition();
+    const [pending, startTransition] = useTransition();
 
-  return (
-    <Button
-      variant="ghost"
-      disabled={pending}
-      onClick={() => startTransition(logoutAction)}
-    >
-      <CustomDropDownMenu.ItemLabel
-        label={pending ? "Chiqilmoqda..." : "Chiqish"}
-        icon={LogoutIcon}
-      />
-    </Button>
-  );
+    return (
+        <Button
+            variant="secondary"
+            disabled={pending}
+            onClick={() => startTransition(logoutAction)}
+            className="border-none w-full [&>div]:mr-auto !bg-transparent p-0"
+        >
+            <CustomDropDownMenu.ItemLabel
+                label={<span className="text-[var(--destructive)]">{pending ? "Chiqilmoqda..." : "Chiqish"}</span>}
+                icon={LogoutIcon}
+            />
+        </Button>
+    );
 }
